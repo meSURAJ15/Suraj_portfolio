96472a40e0c1b72b51f585d5b0084e2d1284749f
meSURAJ15 <129860150+meSURAJ15@users.noreply.github.com>
2024-01-02 01:23:55 +0530

    removed form submission

--- script.js
+++ script.js
@@ -35,18 +35,18 @@ $(document).ready(function(){
   });
 
 });
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzFNj4HANEGgtfmX_Ar6V-OFfBxmE7RlcYk7VrOoUOBdNEH8bAZo08IhDupqHpbo8vWtQ/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg")
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => {
-            msg.innerHTML = "Message sent successfully"
-            setTimeout(function () {
-                msg.innerHTML = ""
-            }, 5000)
-            form.reset()
-        })
-        .catch(error => console.error('Error!', error.message))
-})
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbzFNj4HANEGgtfmX_Ar6V-OFfBxmE7RlcYk7VrOoUOBdNEH8bAZo08IhDupqHpbo8vWtQ/exec'
+// const form = document.forms['submit-to-google-sheet']
+// const msg = document.getElementById("msg")
+// form.addEventListener('submit', e => {
+//     e.preventDefault()
+//     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+//         .then(response => {
+//             msg.innerHTML = "Message sent successfully"
+//             setTimeout(function () {
+//                 msg.innerHTML = ""
+//             }, 5000)
+//             form.reset()
+//         })
+//         .catch(error => console.error('Error!', error.message))
+// })
